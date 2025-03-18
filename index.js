c8d231abcf1711f0353fba12fcfcb3696c864b31
Jesse Kipsang <kipsangjesse17@gmail.com>
2025-03-18 10:20:10 +0300

    updated index.js as specified by instructions

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-function addingEventListener() {
+function addingEventListener() {const input = document.getElementById('button');
+
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+    
+    input.addEventListener('click', clickAlert);
+    
+    const clicker = document.getElementById('main');
+    
+    function clickMessage() {
+        alert('I was touched');
+    }
+    
+    clicker.addEventListener('click', clickMessage);
+    
 }
